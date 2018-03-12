f19ba29159c64be68dec51e9310292217ca5092e
Alexandre BENARD <alben59dk@gmail.com>
2018-03-12 14:39:11 +0100

    add tag

---
+++ wild.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+      coucou les gens.
+      Je test les tag
+    </p>
+  </body>
+</html>
